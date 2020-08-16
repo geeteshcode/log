1b0fe22f013212e472294f3989ca6ec67f3ad287
geetesh <geetesh.popli@gmail.com>
2020-08-16 20:17:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Node.js & Passport Login
+
+```sh
+$ npm install
+```
+npm nodemon app.js
+ http://localhost:4000
+```
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://geet_16:geetesh@cluster0.f0jyk.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
